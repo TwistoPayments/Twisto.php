48499025227daa79512e770ad6ffdcaf908e619e
Martin Jahn <martin.jahn@twisto.cz>
2014-10-10 14:00:04 +0200

    added company_id and vat_id

--- src/Twisto/Customer.php
+++ src/Twisto/Customer.php
@@ -14,16 +14,26 @@ class Customer
     /** @var string */
     public $facebook_id;
 
+    /** @var string */
+    public $company_id;
+
+    /** @var string */
+    public $vat_id;
+
     /**
      * @param string $email
      * @param string $name
      * @param string $facebook_id
+     * @param string $company_id
+     * @param string $vat_id
      */
-    public function __construct($email, $name=null, $facebook_id=null)
+    public function __construct($email, $name=null, $facebook_id=null, $company_id=null, $vat_id=null)
     {
         $this->email = $email;
         $this->name = $name;
         $this->facebook_id = $facebook_id;
+        $this->company_id = $company_id;
+        $this->vat_id = $vat_id;
     }
 
     /**
@@ -34,7 +44,9 @@ class Customer
         return array(
             'email' => $this->email,
             'name' => $this->name,
-            'facebook_id' => $this->facebook_id
+            'facebook_id' => $this->facebook_id,
+            'company_id' => $this->company_id,
+            'vat_id' => $this->vat_id
         );
     }
 }
